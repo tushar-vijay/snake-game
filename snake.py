38de97e2a7b1f3d7c1fae1674587823eb96c41e4
Tushar Vijay <97074208+tushar-vijay@users.noreply.github.com>
2023-09-11 17:03:12 +0530

    Add files via upload

---
+++ snake.py
@@ -0,0 +1,67 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            # creating the snake with three turtles
+            self.add_segment(position)
+
+        # segment_1 = Turtle("square")
+        # segment_1.color("white")
+        #
+        # segment_2 = Turtle("square")
+        # segment_2.color("white")
+        # segment_2.goto(-20, 0)
+        #
+        # segment_3 = Turtle("square")
+        # segment_3.color("white")
+        # segment_3.goto(-40, 0)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            # tail following the head
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
